5c438089c9960598fab917b24d072a4f546e5029
Destro <59611471+destro-code@users.noreply.github.com>
2025-08-06 14:51:08 +0100

    Update React.ts

--- 0x04-TypeScript/task_4/js/subjects/React.ts
+++ 0x04-TypeScript/task_4/js/subjects/React.ts
@@ -1,5 +1,5 @@
-/// <reference path="./Teacher.ts"/>
-/// <reference path="./Subject.ts"/>
+/// <reference path="./Teacher.ts" />
+/// <reference path="./Subject.ts" />
 
 namespace Subjects {
   export class React extends Subject {
