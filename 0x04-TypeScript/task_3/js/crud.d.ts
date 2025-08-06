7036493a6ee44b0651e0d4c79ebfad62cf04efb2
Destro <59611471+destro-code@users.noreply.github.com>
2025-08-06 14:35:09 +0100

    Update crud.d.ts

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -1,7 +1,7 @@
 import { RowID, RowElement } from './interface';
 
-declare module './crud' {
-  export function insertRow(row: RowElement): RowID;
-  export function deleteRow(rowId: RowID): void;
-  export function updateRow(rowId: RowID, row: RowElement): RowID;
-}
+declare function insertRow(row: RowElement): number;
+declare function deleteRow(rowId: RowID): void;
+declare function updateRow(rowId: RowID, row: RowElement): number;
+
+export { insertRow, deleteRow, updateRow };
